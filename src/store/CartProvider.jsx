095b1502e4a4a1ceedb5a018125bdcff86fa66db
Mona Khorasani <monakhrsn@gmail.com>
2024-02-01 15:32:35 +0100

    Implementing the base of addCardReducer

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,12 +1,38 @@
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+  items: [],
+  totalAmount:0
+}
+
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    //using concat method to be able to return a new array
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.iem.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  } 
+
+  return defaultCartState;
+}
+
 const CartProvider = props => {
-  const addItemToCartHandler = item => {};
-  const removeItemFromCartHandler = id => {};
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+  const addItemToCartHandler = item => {
+    dispatchCartAction({type: 'ADD', item: item})
+  };
+  const removeItemFromCartHandler = id => {
+    dispatchCartAction({type: 'REMOVE', id: id})
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
